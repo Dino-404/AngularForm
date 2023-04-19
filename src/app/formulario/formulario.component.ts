dab38e0d340ac74a2fd0db3c82abb5eb50959ca9
VASS\guillermo.cepeda <guillermo.cepeda@vass.es>
2023-04-19 11:52:12 +0200

    Formulario actualizado con password, confirmar password y sus validaciones, aparte validacion de DNI

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -15,13 +15,33 @@ export class FormularioComponent {
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
       correo: ['', [Validators.required, Validators.email]],
-      dni: ['', [Validators.required, Validators.pattern('[0-9]{8}')]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(24),]],
+      confirmPassword: ['', Validators.required],
+      dni: ['', [Validators.maxLength(9), Validators.required, Validators.pattern('^(?:(?:([XYZ])\\d{7,8})|(\\d{8})([A-HJ-NP-TV-Z]))$')]],
       fechaNacimiento: ['', Validators.required]
-    })}
+    }
+      , {
+        validator: this.passwordMatchValidator
+      });}
 
   enviarFormulario() {
     if (this.formulario.valid) {
       alert(JSON.stringify(this.formulario.value));
+    }
+  }
+
+  passwordMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    if (password?.value !== confirmPassword?.value) {
+      confirmPassword?.setErrors({ passwordMismatch: true });
+      return { passwordMismatch: true };
+    } else {
+      confirmPassword?.setErrors(null);
+      return null;
     }
   }
 
